98380790f06db45212299498ca42a6dc8b54bd98
beingelvis <owitelvis@gmail.com>
2023-03-22 21:10:28 +0300

    Demore

---
+++ main.js
@@ -0,0 +1,8 @@
+function showheader(outputId){
+    document.getElementById("output1").style.display = "none";
+    document.getElementById("output2").style.display = "none";
+    document.getElementById("output3").style.display = "none";
+    document.getElementById("output4").style.display = "none";
+
+    document.getElementById(outputId).style.display = "block";
+}
